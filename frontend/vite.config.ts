ed37073521976900164f778ddae83198a2924c65
Brad Ko <javarako@gmail.com>
2026-07-30 17:47:33 -0400

    add new dns

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
+    allowedHosts: ['church.operation.mooo.com'],
     proxy: {
       '/api': 'http://backend:8080',
       '/actuator': 'http://backend:8080',
