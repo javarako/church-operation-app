479477c277fac9e6d86887ab5ad29d407e24ae56
Brad Ko <javarako@gmail.com>
2026-07-30 18:12:32 -0400

    feat: configure frontend hostname per deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
-    allowedHosts: ['church.operation.mooo.com'],
     proxy: {
       '/api': 'http://backend:8080',
       '/actuator': 'http://backend:8080',
